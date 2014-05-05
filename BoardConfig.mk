8e2d13501990f05ef99d6f78c8152b0a9ffa0069
xiangxin <xiangxin19960319@gmail.com>
2014-05-05 21:24:27 +0800

    clean up

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,60 +1,19 @@
-#
-# Copyright (C) 2013 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-# Copyright (C) 2014 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
-
--include device/zte/msm8974-common/BoardConfigCommon.mk
-
 # inherit from the proprietary version
 -include vendor/zte/nx503a/BoardConfigVendor.mk
 
-TARGET_BOOTLOADER_NAME		 := NX503A
+-include device/zte/msm8974-common/BoardConfigCommon.mk
+
+TARGET_BOOTLOADER_NAME := NX503A
 TARGET_BOOTLOADER_BOARD_NAME := MSM8974
 
 TARGET_BOARD_INFO_FILE := device/zte/nx503a/board-info.txt
 
-BOARD_KERNEL_CMDLINE 	:= console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 
 #androidboot.selinux=permissive
 
-BOARD_KERNEL_BASE 		:= 0x00000000
-BOARD_KERNEL_PAGESIZE 	:= 2048
-BOARD_MKBOOTIMG_ARGS 	:= --ramdisk_offset 0x2000000 --tags_offset 0x01E00000 
-
-
-BOARD_BOOTIMAGE_PARTITION_SIZE 		:= 10485760		#10M  boot
-BOARD_RECOVERYIMAGE_PARTITION_SIZE 	:= 10485760		#10M  recovery
-BOARD_SYSTEMIMAGE_PARTITION_SIZE 	:= 1288488960	#1.2G system
-BOARD_USERDATAIMAGE_PARTITION_SIZE 	:= 2684354560	#2.5G data
-BOARD_FLASH_BLOCK_SIZE 				:= 131072
-
-#TARGET_PREBUILT_KERNEL := device/zte/nx503a/kernel
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x2000000 --tags_offset 0x01E00000 
 
 TARGET_KERNEL_SOURCE := kernel/zte/nx503a
 TARGET_KERNEL_CONFIG := cm_nx503a_defconfig
@@ -65,7 +24,12 @@ TARGET_ZTEMT_DTS := true
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_CUSTOM_BOOTIMG_MK := device/zte/nx503a/mkbootimg.mk
 
-#TARGET_KERNEL_CUSTOM_TOOLCHAIN:= linaro-4.8
+
+BOARD_BOOTIMAGE_PARTITION_SIZE := 10485760
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10485760
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1288488960
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2684354560
+BOARD_FLASH_BLOCK_SIZE := 131072
 
 #RIL
 BOARD_RIL_CLASS := ../../../device/zte/nx503a/ril/
@@ -99,34 +63,9 @@ TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_RECOVERY_ALWAYS_WIPES := true
 BOARD_RECOVERY_HANDLES_MOUNT := true
 BOARD_USES_MMCUTILS := true
-
-
-
-BOARD_RECOVERY_SWIPE 				:= true
-RECOVERY_FSTAB_VERSION 				:= 2
-TARGET_RECOVERY_FSTAB 				:= device/zte/nx503a/ramdisk/fstab.qcom
-TARGET_RECOVERY_PIXEL_FORMAT 		:= "RGBX_8888"
-BOARD_USE_CUSTOM_RECOVERY_FONT 		:= \"roboto_23x41.h\"
-
-
-#TWRP config
-DEVICE_RESOLUTION 					:= 1080x1920
-RECOVERY_GRAPHICS_USE_LINELENGTH 	:= true
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-TW_INCLUDE_JB_CRYPTO 				:= true
-TW_FLASH_FROM_STORAGE 				:= true
-BOARD_HAS_NO_REAL_SDCARD 			:= false
-RECOVERY_SDCARD_ON_DATA 			:= false
-
-TW_INTERNAL_STORAGE_PATH 			:= "/storage/sdcard"
-TW_INTERNAL_STORAGE_MOUNT_POINT 	:= "/sdcard"
-
-
-TW_BRIGHTNESS_PATH 					:= "/sys/class/leds/lcd-backlight/brightness"
-TW_MAX_BRIGHTNESS 					:= 100
-
-TW_NO_SCREEN_TIMEOUT 				:= true
-#TWRP end
-
-
+BOARD_RECOVERY_SWIPE := true
+RECOVERY_FSTAB_VERSION := 2
+TARGET_RECOVERY_FSTAB := device/zte/nx503a/ramdisk/fstab.qcom
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 
