55ef083a0bbf776db04c4d109ab34e8bab7a8a2c
xiangxin <xiangxin19960319@gmail.com>
2014-05-06 23:33:45 +0800

    Revert "prebuilt kernel"
    This reverts commit 4482d044afb3ad8c63e83f23034bc96621ffa8b6.

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -1,5 +1,3 @@
-
-ifneq ($(TARGET_ZTEMT_DTS),false)
 uncompressed_ramdisk := $(PRODUCT_OUT)/ramdisk.cpio
 $(uncompressed_ramdisk): $(INSTALLED_RAMDISK_TARGET)
 	zcat $< > $@
@@ -25,22 +23,16 @@ endef
 DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbToolCM$(HOST_EXECUTABLE_SUFFIX)
 INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
 
-
 $(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr $(INSTALLED_KERNEL_TARGET)
 	@echo -e ${CL_CYN}"Start DT image: $@"${CL_RST}
 	$(call append-dtb)
 	$(call pretty,"Target dt image: $(INSTALLED_DTIMAGE_TARGET)")
 	$(hide) $(DTBTOOL) -o $(INSTALLED_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/arm/boot/
 	@echo -e ${CL_CYN}"Made DT image: $@"${CL_RST}
-
-else
-INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/../../../../device/zte/$(TARGET_DEVICE)/prebuilt/dt.img
-endif
-
+	
 ## Overload bootimg generation: Same as the original, + --dt arg
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTALLED_DTIMAGE_TARGET)
 	$(call pretty,"Target boot image: $@")
-	$(hide) cp -R $(PRODUCT_OUT)/../../../../device/zte/$(TARGET_DEVICE)/prebuilt/modules/* $(PRODUCT_OUT)/system/lib/modules/
 	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
 	@echo -e ${CL_CYN}"Made boot image: $@"${CL_RST}
