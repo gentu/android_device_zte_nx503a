d7ea4ba677ee9268c4d29174539267365fb7f5df
XianGxin <xiangxin19960319@gmail.com>
2014-05-14 13:01:52 +0800

    Call phone-xxhdpi-2048-hwui-memory.mk

--- nx503a.mk
+++ nx503a.mk
@@ -344,6 +344,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 # call dalvik heap config
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 # Wifi bcm4339
 $(call inherit-product, hardware/broadcom/wlan/bcmdhd/firmware/bcm4339/device-bcm.mk)
