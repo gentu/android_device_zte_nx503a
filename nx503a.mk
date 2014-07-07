a79e1926f63f32c06a1f271c608880edc4982a25
Maxim Lapunin <majagyage@gmail.com>
2014-07-08 01:33:25 +0400

    Add wifi overlay configs

--- nx503a.mk
+++ nx503a.mk
@@ -248,6 +248,9 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
+# WiFi overlay configs
+$(call inherit-product, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
+
 # Inherit from vendor files
 $(call inherit-product, vendor/zte/nx503a/nx503a-vendor.mk)
 
